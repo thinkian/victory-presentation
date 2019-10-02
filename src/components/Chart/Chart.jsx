8ca93aad73dd0ac8339dd1b199a6479ed6225ebf
Ian McPhail <ian.mcphail@thinkcompany.com>
2019-10-02 13:47:27 -0400

    Add VictoryBar with VictoryAxis

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,16 +1,68 @@
 import React, { Component } from 'react';
+import { VictoryAxis, VictoryBar, VictoryChart } from 'victory';
 
 import './Chart.css';
 
 import MOCK_DATA from 'data/mock1';
 
 class Chart extends Component {
+  get axes() {
+    return [
+      <VictoryAxis
+        key="xAxis"
+        tickValues={this.xValues}
+        tickFormat={d => `Quarter ${d}`}
+      />,
+      <VictoryAxis
+        key="yAxis"
+        dependentAxis
+        tickFormat={d => `$${d / 1000}k`}
+      />
+    ];
+  }
+
+  get barStyle() {
+    return {
+      data: {
+        fill: 'mediumseagreen'
+      }
+    };
+  }
+
+  get chartPadding() {
+    return {
+      top: 0,
+      bottom: 25,
+      left: 50,
+      right: 50
+    };
+  }
+
   get data() {
     return MOCK_DATA.data;
   }
 
+  get xValues() {
+    return this.data.map(datum => datum.quarter);
+  }
+
   render() {
-    return <div className="my-chart">Award winning chart goes here</div>;
+    return (
+      <div className="my-chart">
+        <VictoryChart
+          domainPadding={20}
+          height={200}
+          padding={this.chartPadding}>
+          {this.axes}
+          <VictoryBar
+            data={this.data}
+            x="quarter"
+            y="earnings"
+            style={this.barStyle}
+          />
+        </VictoryChart>
+      </div>
+    );
   }
 }
 
